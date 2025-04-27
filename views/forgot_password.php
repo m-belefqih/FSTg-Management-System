0d77f7395df7a763034d7abad65f1fe5374babbd
BELEFQIH Mohammed <mblq02@gmail.com>
2025-04-27 22:40:55 +0100

    Update forgot_password.php

---
+++ views/forgot_password.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>FSTG - Login</title>
+</head>
+<body>
+    <h1>Page pour changer votre mot de passe</h1>
+</body>
+</html>
