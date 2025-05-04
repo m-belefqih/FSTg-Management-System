0350469d5b5138db2d1bc4807ca8104a0acb2448
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-05 00:35:24 +0100

    Update home.php of student

--- views/student/home.php
+++ views/student/home.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+require($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/auth/session.php');
+
+if(!isset($_SESSION['user_data'])) {
+    header("Location: /FSTg-Management-System/index.php");
+    exit();
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,5 +17,15 @@
 </head>
 <body>
     <h1>Je suis un étudiant</h1>
+
+    <p>Mon id : <?php echo $_SESSION['user_data']['id'] ?> </p>
+    <p>Mon nom : <?php echo $_SESSION['user_data']['nom'] ?> </p>
+    <p>Mon prenom : <?php echo $_SESSION['user_data']['prenom'] ?> </p>
+    <p>Mon CIN : <?php echo $_SESSION['user_data']['CIN'] ?> </p>
+    <p>Mon role : <?php echo $_SESSION['user_data']['id_role'] ?> </p>
+    <p>Mon département : <?php echo $_SESSION['user_data']['id_dep'] ?> </p>
+    <p>Mon filiere : <?php echo $_SESSION['user_data']['id_filiere'] ?> </p>
+    <p>Le nom de rôle : <?php echo $_SESSION['role_name'] ?></p>
+    <a href="../../logout.php">Se déconnecter</a>
 </body>
 </html>
