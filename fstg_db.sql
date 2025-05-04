be5775818283692ee5ca5705e744c0ee1605f9c0
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-04 16:17:31 +0100

    Update foreign key constraints and improve SQL data insertion format

--- fstg_db.sql
+++ fstg_db.sql
@@ -40,11 +40,10 @@ CREATE TABLE `user` (
   `CNE` varchar(20) DEFAULT NULL UNIQUE,
   `CIN` varchar(20) NOT NULL UNIQUE,
   `phone` varchar(20) DEFAULT NULL UNIQUE,
-  `niveau` int DEFAULT NULL,  -- Niveau est l'année d'étude (soit 1er année soit 2ème année ...)
   `created_at` datetime NOT NULL,
   `id_role` int(11) NOT NULL,
   `id_dep` int(11) NOT NULL,
-  `id_filiere` int(11) DEFAULT NULL,
+  `id_filiere` int(11) DEFAULT NULL,        -- la filière de l'étudiant
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -52,23 +51,23 @@ CREATE TABLE `user` (
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `prenom`, `nom`, `genre`, `dateNaissance`, `email`, `password`, `CNE`, `CIN`, `phone`, `niveau`, `created_at`, `id_filiere`, `id_role`, `id_dep`) VALUES
-(1, 'Soulaimane ', 'Kaloun', 'male', NULL, 'so.kaloun@uca.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12345', NULL, NULL, '2025-04-29 15:04:10', NULL, 1, 1),
-(2, 'QASSIMI', 'Sara', 'female', NULL, 'sara.qassimi@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12346', NULL, NULL, '2025-04-29 15:04:10', NULL, 2, 1),
-(3, 'SADQI', 'Yassine', 'male', NULL, 'y.sadqi@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12347', NULL, NULL, '2025-04-29 15:04:10', NULL, 2, 1),
-(4, 'DAROUICHI', 'Aziz', 'male', NULL, 'az.darouichi@uca.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12348', NULL, NULL, '2025-04-29 15:04:10', NULL, 3, 1),
-(5, 'AIT HEMAD', 'Mohammed', 'male', NULL, 'ait.hemad.m@hotmail.com', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12349', NULL, NULL, '2025-04-29 15:04:10', NULL, 3, 1),
-(6, 'BELEFQIH', 'Mohammed', 'male', NULL, 'm.belefqih5955@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12350', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(7, 'MOSSAAB', 'SADKI', 'male', NULL, 'm.sadki9752@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12351', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(8, 'HAMZA', 'OUTZMOURTE', 'male', NULL, 'h.outzmourte1206@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12352', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(9, 'ANAS', 'HALLOUMI', 'male', NULL, 'a.halloumi9030@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12353', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(10, 'MOHAMMED', 'EL OMRI', 'male', NULL, 'm.elomri0337@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12354', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(11, 'MOHAMMED', 'ALLALI', 'male', NULL, 'm.allali1846@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12355', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(12, 'NOURA', 'LAMZARA', 'female', NULL, 'n.lamzara2439@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12356', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(13, 'SAYOUBA', 'OUEDRAOGO', 'male', NULL, 's.ouedraogo0058@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12357', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(14, 'ABDELILAH', 'ERRAGUIBI', 'male', NULL, 'a.erraguibi3361@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12359', NULL, NULL, '2025-04-29 15:04:10', 1, 4, 1),
-(15, 'Nouhaila', 'EL HAMAL', 'female', NULL, 'n.elhamal4829@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12360', NULL, NULL, '2025-04-29 15:04:10', 4, 4, 1),
-(16, 'Mohammed', 'EL IDRISSI', 'male', NULL, 'm.elidrissi6618@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12361', NULL, NULL, '2025-04-29 15:04:10', 4, 4, 1);
+INSERT INTO `user` (`id`, `prenom`, `nom`, `genre`, `dateNaissance`, `email`, `password`, `CNE`, `CIN`, `phone`, `created_at`, `id_filiere`, `id_role`, `id_dep`) VALUES
+(1, 'Soulaimane ', 'Kaloun', 'male', NULL, 'so.kaloun@uca.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12345', NULL, '2025-04-29 15:04:10', NULL, 1, 1),
+(2, 'QASSIMI', 'Sara', 'female', NULL, 'sara.qassimi@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12346', NULL, '2025-04-29 15:04:10', NULL, 2, 1),
+(3, 'SADQI', 'Yassine', 'male', NULL, 'y.sadqi@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12347', NULL, '2025-04-29 15:04:10', NULL, 2, 1),
+(4, 'DAROUICHI', 'Aziz', 'male', NULL, 'az.darouichi@uca.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12348', NULL, '2025-04-29 15:04:10', NULL, 3, 1),
+(5, 'AIT HEMAD', 'Mohammed', 'male', NULL, 'ait.hemad.m@hotmail.com', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12349', NULL, '2025-04-29 15:04:10', NULL, 3, 1),
+(6, 'BELEFQIH', 'Mohammed', 'male', NULL, 'm.belefqih5955@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12350', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(7, 'MOSSAAB', 'SADKI', 'male', NULL, 'm.sadki9752@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12351', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(8, 'HAMZA', 'OUTZMOURTE', 'male', NULL, 'h.outzmourte1206@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12352', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(9, 'ANAS', 'HALLOUMI', 'male', NULL, 'a.halloumi9030@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12353', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(10, 'MOHAMMED', 'EL OMRI', 'male', NULL, 'm.elomri0337@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12354', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(11, 'MOHAMMED', 'ALLALI', 'male', NULL, 'm.allali1846@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12355', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(12, 'NOURA', 'LAMZARA', 'female', NULL, 'n.lamzara2439@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12356', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(13, 'SAYOUBA', 'OUEDRAOGO', 'male', NULL, 's.ouedraogo0058@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12357', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(14, 'ABDELILAH', 'ERRAGUIBI', 'male', NULL, 'a.erraguibi3361@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12359', NULL, '2025-04-29 15:04:10', 1, 4, 1),
+(15, 'Nouhaila', 'EL HAMAL', 'female', NULL, 'n.elhamal4829@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12360', NULL, '2025-04-29 15:04:10', 4, 4, 1),
+(16, 'Mohammed', 'EL IDRISSI', 'male', NULL, 'm.elidrissi6618@uca.ac.ma', 'fc5e038d38a57032085441e7fe7010b0', NULL, 'X12361', NULL, '2025-04-29 15:04:10', 4, 4, 1);
 
 -- ---------------------------------------------------------------------------------
 
@@ -81,7 +80,7 @@ CREATE TABLE `user_tokens` (
   `selector` varchar(255) NOT NULL,
   `hashed_validator` varchar(255) NOT NULL,
   `expiry` datetime NOT NULL,
-  `user_id` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -139,10 +138,10 @@ CREATE TABLE `filiere` (
   `id` int(11) NOT NULL AUTO_INCREMENT UNIQUE,
   `nom` varchar(30) NOT NULL,
   `nom_complet` varchar(200) NOT NULL,
-  `type` varchar(100) NOT NULL,    -- Exemple : 'Licence', 'Master', 'Cycle d’ingénieur'
+  `type` varchar(100) NOT NULL,         -- Exemple : 'Licence', 'Master', 'Cycle d’ingénieur'
   `niveau` int(11) NOT NULL,            -- Niveau est l'année d'étude (soit 1er année soit 2ème année ...)
-  `id_coordinator` int(11) NOT NULL,
-  `id_dep` int(11) NOT NULL,
+  `id_coordinator` int(11) NOT NULL,    -- le coordinateur qui s'occupe de la filière
+  `id_dep` int(11) NOT NULL,            -- le département qui contient la filière
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -168,8 +167,8 @@ CREATE TABLE `module` (
   `id` int(11) NOT NULL AUTO_INCREMENT UNIQUE,
   `nom` varchar(100) NOT NULL,
   `semestre` enum('1','2') NOT NULL,
-  `id_filiere` int(11) NOT NULL,
-  `id_teacher` int(11) NOT NULL,
+  `id_filiere` int(11) NOT NULL,    -- la filière qui contient le module
+  `id_teacher` int(11) NOT NULL,    -- le professeur qui enseigne le module
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -208,39 +207,39 @@ CREATE TABLE `note` (
 --
 
 INSERT INTO `note` (`id`, `value`, `valideProf`, `valideCoord`, `id_module`, `id_teacher`, `cne_etudiant`) VALUES
-(1, 12, 1, 1, 24, 4, 'C12345678'),
-(2, 10, 1, 1, 24, 4, 'C12345679'),
-(3, 14, 1, 1, 24, 4, 'C12345611'),
-(4, 12, 1, 1, 24, 4, 'C12345617'),
-(5, 12, 1, 1, 24, 4, 'C12345618'),
-(6, 12, 1, 1, 24, 4, 'C12345619'),
-(7, 12, 1, 1, 24, 4, 'C12345620'),
-(8, 12, 1, 1, 24, 4, 'C12345621'),
-(9, 12, 1, 1, 24, 4, 'C12345622'),
-(10, 12, 1, 1, 24, 4, 'C12345623'),
-(11, 12, 1, 1, 24, 4, 'C12345624'),
-(12, 12, 1, 1, 22, 4, 'C12345678'),
-(13, 10, 1, 1, 22, 4, 'C12345679'),
-(14, 14, 1, 1, 22, 4, 'C12345611'),
-(15, 12, 1, 1, 22, 4, 'C12345617'),
-(16, 12, 1, 1, 22, 4, 'C12345618'),
-(17, 12, 1, 1, 22, 4, 'C12345619'),
-(18, 12, 1, 1, 22, 4, 'C12345620'),
-(19, 12, 1, 1, 22, 4, 'C12345621'),
-(20, 12, 1, 1, 22, 4, 'C12345622'),
-(21, 12, 1, 1, 22, 4, 'C12345623'),
-(22, 12, 1, 1, 22, 4, 'C12345624'),
-(23, 12, 1, 1, 25, 4, 'C12345678'),
-(24, 10, 1, 1, 25, 4, 'C12345679'),
-(25, 14, 1, 1, 25, 4, 'C12345611'),
-(26, 12, 1, 1, 25, 4, 'C12345617'),
-(27, 12, 1, 1, 25, 4, 'C12345618'),
-(28, 12, 1, 1, 25, 4, 'C12345619'),
-(29, 12, 1, 1, 25, 4, 'C12345620'),
-(30, 12, 1, 1, 25, 4, 'C12345621'),
-(31, 12, 1, 1, 25, 4, 'C12345622'),
-(32, 12, 1, 1, 25, 4, 'C12345623'),
-(33, 12, 1, 1, 25, 4, 'C12345624');
+(1, 12, 1, 1, 2, 4, 'C12345678'),
+(2, 10, 1, 1, 2, 4, 'C12345679'),
+(3, 14, 1, 1, 2, 4, 'C12345611'),
+(4, 12, 1, 1, 2, 4, 'C12345617'),
+(5, 12, 1, 1, 2, 4, 'C12345618'),
+(6, 12, 1, 1, 2, 4, 'C12345619'),
+(7, 12, 1, 1, 2, 4, 'C12345620'),
+(8, 12, 1, 1, 2, 4, 'C12345621'),
+(9, 12, 1, 1, 2, 4, 'C12345622'),
+(10, 12, 1, 1, 7, 4, 'C12345623'),
+(11, 12, 1, 1, 7, 4, 'C12345624'),
+(12, 12, 1, 1, 7, 4, 'C12345678'),
+(13, 10, 1, 1, 7, 4, 'C12345679'),
+(14, 14, 1, 1, 7, 4, 'C12345611'),
+(15, 12, 1, 1, 7, 4, 'C12345617'),
+(16, 12, 1, 1, 7, 4, 'C12345618'),
+(17, 12, 1, 1, 7, 4, 'C12345619'),
+(18, 12, 1, 1, 7, 4, 'C12345620'),
+(19, 12, 1, 1, 7, 4, 'C12345621'),
+(20, 12, 1, 1, 7, 4, 'C12345622'),
+(21, 12, 1, 1, 7, 4, 'C12345623'),
+(22, 12, 1, 1, 7, 4, 'C12345624'),
+(23, 12, 1, 1, 3, 4, 'C12345678'),
+(24, 10, 1, 1, 3, 4, 'C12345679'),
+(25, 14, 1, 1, 3, 4, 'C12345611'),
+(26, 12, 1, 1, 3, 4, 'C12345617'),
+(27, 12, 1, 1, 3, 4, 'C12345618'),
+(28, 12, 1, 1, 3, 4, 'C12345619'),
+(29, 12, 1, 1, 3, 4, 'C12345620'),
+(30, 12, 1, 1, 3, 4, 'C12345621'),
+(31, 12, 1, 1, 3, 4, 'C12345622'),
+(32, 12, 1, 1, 3, 4, 'C12345623'),
+(33, 12, 1, 1, 3, 4, 'C12345624');
 
 -- ---------------------------------------------------------------------------------
 
@@ -252,23 +251,23 @@ INSERT INTO `note` (`id`, `value`, `valideProf`, `valideCoord`, `id_module`, `id
 -- Constraints for table `user_tokens`
 --
 ALTER TABLE `user_tokens`
-    ADD CONSTRAINT `user_tokens_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `user_tokens_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `user`
 --
 
 ALTER TABLE `user`
-    ADD CONSTRAINT `user_fk1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `user_fk2` FOREIGN KEY (`id_dep`) REFERENCES `departement` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `user_fk3` FOREIGN KEY (`id_filiere`) REFERENCES `filiere` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT `user_fk1` FOREIGN KEY (`id_role`) REFERENCES `role` (`id`) ON UPDATE CASCADE,
+    ADD CONSTRAINT `user_fk2` FOREIGN KEY (`id_dep`) REFERENCES `departement` (`id`) ON UPDATE CASCADE,
+    ADD CONSTRAINT `user_fk3` FOREIGN KEY (`id_filiere`) REFERENCES `filiere` (`id`) ON UPDATE CASCADE;
 
 --                                                                                            --
 -- Constraints for table `filiere`
 --
 
 ALTER TABLE `filiere`
-    ADD CONSTRAINT `filiere_fk1` FOREIGN KEY (`id_coordinator`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+    ADD CONSTRAINT `filiere_fk1` FOREIGN KEY (`id_coordinator`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     ADD CONSTRAINT `filiere_fk2` FOREIGN KEY (`id_dep`) REFERENCES `departement` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --                                                                                            --
@@ -284,7 +283,6 @@ ALTER TABLE `module`
 --
 
 ALTER TABLE `note`
-    ADD CONSTRAINT `note_fk1` FOREIGN KEY (`id_module`) REFERENCES `module` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-    ADD CONSTRAINT `note_fk2` FOREIGN KEY (`id_teacher`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
-    ADD CONSTRAINT `note_fk3` FOREIGN KEY (`cne_etudiant`) REFERENCES `user` (`CNE`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `note_fk1` FOREIGN KEY (`id_module`) REFERENCES `module` (`id`) ON UPDATE CASCADE,
+    ADD CONSTRAINT `note_fk2` FOREIGN KEY (`id_teacher`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
 COMMIT;
