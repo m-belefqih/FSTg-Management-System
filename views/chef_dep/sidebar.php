12dfdb83103453d519b39d2328a1bc5c0f300a29
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-05 17:35:00 +0100

    Translate sidebar menu items to French and update role check

--- views/chef_dep/sidebar.php
+++ views/chef_dep/sidebar.php
@@ -4,35 +4,39 @@
             <div id="sidebar-menu" class="sidebar-menu">
                 <ul>
                     <li class="menu-title">
-                        <span>Main Menu</span>
+                        <span>Menu Principal</span>
                     </li>
                     <li>
-                        <a href="home.php"><i class="fas fa-home"></i><span> Home</span></a>
+                        <a href="home.php"><i class="fas fa-home"></i><span> Accueil</span></a>
                     </li>
                     <li>
-                        <a href="teacher_management/list_teachers.php"><i class="fas fa-chalkboard-teacher"></i><span> Teachers</span></a>
+                        <a href="teacher_management/list_teachers.php"><i class="fas fa-chalkboard-teacher"></i><span> Enseignants</span></a>
                     </li>
                     <li>
                         <a href="module_management/list_modules.php"><i class="fas fa-book-reader"></i><span> Modules</span></a>
                     </li>
-                    <?php  if ($_SESSION['user_data']['role'] == 1) { ?>
+                    <?php if ($_SESSION['user_data']['id_role'] == 1) { ?>
                         <li class="menu-title">
-                            <span>Profesor Menu</span>
+                            <span>Menu Professeur</span>
                         </li>
                         <li>
-                            <a href="/ENSAHify/views/professeur/choixModuleCours.php"><i class="fas fa-book"></i> <span> Cours</span></a>
+<!--                            <a href="/ENSAHify/views/professeur/choixModuleCours.php"><i class="fas fa-book"></i> <span> Cours</span></a>-->
+                            <a href="#"><i class="fas fa-book"></i> <span> Cours</span></a>
                         </li>
                         <li>
-                            <a href="/ENSAHify/views/professeur/module-view.php"><i class="fas fa-book-reader"></i><span> Modules List</span></a>
+<!--                            <a href="/ENSAHify/views/professeur/module-view.php"><i class="fas fa-book-reader"></i><span> Liste des Modules</span></a>-->
+                            <a href="#"><i class="fas fa-book-reader"></i><span> Liste des Modules</span></a>
                         </li>
                         <li>
-                            <a href="/ENSAHify/views/professeur/choixModuleNotes.php"><i class="fas fa-clipboard"></i> <span> Grades</span></a>
+<!--                            <a href="/ENSAHify/views/professeur/choixModuleNotes.php"><i class="fas fa-clipboard"></i> <span> Notes</span></a>-->
+                            <a href="#"><i class="fas fa-clipboard"></i> <span> Notes</span></a>
                         </li>
                         <li>
-                            <a href="/ENSAHify/views/professeur/timetable.php"><i class="fas fa-calendar-day"></i> <span> Times Table</span></a>
+<!--                            <a href="/ENSAHify/views/professeur/timetable.php"><i class="fas fa-calendar-day"></i> <span> Emploi du Temps</span></a>-->
+                            <a href="#"><i class="fas fa-calendar-day"></i> <span> Emploi du Temps</span></a>
                         </li>
                         <li>
-                            <a href="#"><i class="fas fa-address-card"></i><span> Profile</span></a>
+                            <a href="#"><i class="fas fa-address-card"></i><span> Profil</span></a>
                         </li>
                     <?php }?>
                 </ul>
