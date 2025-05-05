fbab7e2816e593afc1b83efb7216283db65afe36
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-05 17:34:24 +0100

    Fix asset paths in error.php for correct resource loading

--- views/error.php
+++ views/error.php
@@ -10,19 +10,19 @@
     <!-- CSS Styles -->
     <link rel="shortcut icon" href="https://ecampus-fst.uca.ma/pluginfile.php/1/theme_moove/favicon/1739555045/logo-universite-cadi-ayyad-marrakech-uca%20%281%29.ico">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/plugins/feather/feather.css">
-    <link rel="stylesheet" href="assets/plugins/icons/flags/flags.css">
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
-    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style-override.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/plugins/feather/feather.css">
+    <link rel="stylesheet" href="../assets/plugins/icons/flags/flags.css">
+    <link rel="stylesheet" href="../assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="../assets/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="../assets/css/style-override.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body class="error-page">
 
     <div class="main-wrapper">
         <div class="error-box">
-            <img src="assets/img/error404.svg" style="height: 450px;" alt="logo of 404 page">
+            <img src="../assets/img/error404.svg" style="height: 450px;" alt="logo of 404 page">
             <h3 class="h2 mb-3"><i class="fas fa-exclamation-triangle"></i> Oops! Page not found!</h3>
             <p class="h4 font-weight-normal">The page you requested was not found.</p>
 
@@ -37,9 +37,9 @@
         </div>
     </div>
     <!-- Script files -->
-    <script src="assets/js/jquery-3.6.0.min.js"></script>
-    <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="assets/js/script.js"></script>
+    <script src="../assets/js/jquery-3.6.0.min.js"></script>
+    <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
