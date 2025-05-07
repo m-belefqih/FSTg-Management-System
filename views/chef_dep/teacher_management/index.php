07198de6b943f02be58f5d9d1d255b67c999faad
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-07 15:37:21 +0100

    Add index.php for teacher management routing and session validation

---
+++ views/chef_dep/teacher_management/index.php
@@ -0,0 +1,43 @@
+<?php
+session_start();
+require_once($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/auth/session.php');
+
+if (isset($_SESSION['user_data'])) {
+
+    // Linking the teacher.php model and view using this controller
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/controllers/teacher_controller.php');
+
+    // Creation of a route for teacher management
+    if (isset($_GET['action'])) {
+        $action = $_GET['action'];
+        switch ($action) {
+            case 'create':
+                createAction();
+                break;
+            case 'list':
+                indexAction();
+                break;
+            case 'edit':
+                editAction();
+                break;
+            case 'store':
+                storeAction();
+                break;
+            case 'update':
+                updateAction();
+                break;
+            case 'delete':
+                deleteAction();
+                break;
+            default:
+                header("Location: /FSTg-Management-System/views/error.php");
+                break;
+        }
+    } else {
+        // Default action when no action parameter is provided
+        indexAction(); // Typically shows the list view as default
+    }
+} else {
+    header("Location: /FSTg-Management-System/index.php?error=Accès non autorisé");
+    exit();
+}
