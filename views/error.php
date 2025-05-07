7185434765d4588ae32f39a08a69687f352cd2db
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-07 15:44:58 +0100

    Update error.php to improve role-based navigation and clean up unused styles

--- views/error.php
+++ views/error.php
@@ -11,8 +11,6 @@
     <link rel="shortcut icon" href="https://ecampus-fst.uca.ma/pluginfile.php/1/theme_moove/favicon/1739555045/logo-universite-cadi-ayyad-marrakech-uca%20%281%29.ico">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/plugins/feather/feather.css">
-    <link rel="stylesheet" href="../assets/plugins/icons/flags/flags.css">
     <link rel="stylesheet" href="../assets/plugins/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="../assets/plugins/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="../assets/css/style-override.css">
@@ -26,20 +24,30 @@
             <h3 class="h2 mb-3"><i class="fas fa-exclamation-triangle"></i> Oops! Page not found!</h3>
             <p class="h4 font-weight-normal">The page you requested was not found.</p>
 
-            <a href="<?php if (isset($_SESSION['user_data']['role'])){
-                if ($_SESSION['user_data']['role'] == 2) {
-                    echo "/ENSAHify/views/coordinateur/home.php";
-                } else {
-                    echo "/ENSAHify/views/professeur/home.php";
+            <a href="<?php
+            if (isset($_SESSION['user_data'])) {
+                switch ($_SESSION['user_data']['id_role']) {
+                    case 1: // chef_dep
+                        echo "/FSTg-Management-System/views/chef_dep/home.php";
+                        break;
+                    case 2: // coordinateur
+                        echo "/FSTg-Management-System/views/coordinateur/home.php";
+                        break;
+                    case 3: // professeur
+                        echo "/FSTg-Management-System/views/professeur/home.php";
+                        break;
+                    case 4: // étudiant
+                        echo "/FSTg-Management-System/views/etudiant/home.php";
+                        break;
                 }
             }
             ?>" class="btn btn-primary">Back to Home</a>
         </div>
+
+
     </div>
-    <!-- Script files -->
-    <script src="../assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Script file -->
     <script src="../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
