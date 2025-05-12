cd714302758a17e1e85d29f30dd10938444a3e8c
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-12 10:22:57 +0100

    Update download link in list_modules.php

--- views/chef_dep/module_management/list_modules.php
+++ views/chef_dep/module_management/list_modules.php
@@ -98,7 +98,7 @@
                                         <h3 class="page-title">Modules du Département <?php if(!empty($modules)) { echo $modules[0]['nom_departement'];} ?></h3>
                                     </div>
                                     <div class="col-auto text-end float-end ms-auto download-grp">
-                                        <a href="/FSTg-Management-System/controllers/downloadCSV_controller.php" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Télécharger</a>
+                                        <a href="/FSTg-Management-System/controllers/downloadCSV_controller.php?action=modules" class="btn btn-outline-primary me-2"><i class="fas fa-download"></i> Télécharger</a>
                                         <a href="index.php?action=create" class="btn btn-primary"><i class="fas fa-plus"></i></a>
                                     </div>
                                 </div>
