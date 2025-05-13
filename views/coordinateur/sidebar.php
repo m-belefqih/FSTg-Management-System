1aaa3c16e2996c84480e6dc93266c6103493f63c
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-14 00:34:22 +0100

    Update sidebar.php navigation links for coordinateur role

--- views/coordinateur/sidebar.php
+++ views/coordinateur/sidebar.php
@@ -7,42 +7,35 @@
                         <span>Main Menu</span>
                     </li>
                     <li>
-                        <a href="#"><i class="fas fa-home"></i><span> Accueil</span></a>
+                        <a href="/FSTg-Management-System/views/coordinateur/home.php"><i class="fas fa-home"></i><span> Accueil</span></a>
                     </li>
                     <li>
-                        <a href="#"><i class="fas fa-graduation-cap"></i><span> Étudiants</span></a>
+                        <a href="/FSTg-Management-System/views/coordinateur/student_management/index.php"><i class="fas fa-graduation-cap"></i><span> Étudiants</span></a>
                     </li>
                     <li>
-                        <a href="#"><i
-                                    class="fas fa-chalkboard-teacher"></i><span> Enseignants</span></a>
+                        <a href="#"><i class="fas fa-chalkboard-teacher"></i><span> Enseignants</span></a>
                     </li>
                     <li>
-                        <a href="#"><i
-                                    class="fas fa-tasks"></i><span> Affecter un module</span></a>
+                        <a href="#"><i class="fas fa-tasks"></i><span> Affecter un module</span></a>
                     </li>
 
                     <li class="submenu">
-                        <a href="#"><i class="fas fa-clipboard"></i> <span> Notes</span> <span
-                                    class="menu-arrow"></span></a>
+                        <a href="#"><i class="fas fa-clipboard"></i> <span> Notes</span> <span class="menu-arrow"></span></a>
                         <ul>
-                            <li><a href="#"><i
-                                            class="fa fa-list"></i><span> Liste des notes</span></a></li>
+                            <li><a href="#"><i class="fa fa-list"></i><span> Liste des notes</span></a></li>
                             <li><a href="#"><i class="fa fa-archive"></i><span> Archive des notes</span></a></li>
-                            <li><a href="#"><i
-                                            class="fa fa-file-export"></i><span> Exporter les notes</span></a></li>
+                            <li><a href="#"><i class="fa fa-file-export"></i><span> Exporter les notes</span></a></li>
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i
-                                    class="fas fa-calendar-day"></i><span> Emploi du temps</span></a>
+                        <a href="#"><i class="fas fa-calendar-day"></i><span> Emploi du temps</span></a>
                     </li>
                     <?php if ($_SESSION['user_data']['id_role'] == 2) { ?>
                         <li class="menu-title">
                             <span>Profesor Menu</span>
                         </li>
                         <li class="submenu">
-                            <a href="#"><i class="fas fa-book-reader"></i> <span> Modules</span> <span
-                                        class="menu-arrow"></span></a>
+                            <a href="#"><i class="fas fa-book-reader"></i> <span> Modules</span> <span class="menu-arrow"></span></a>
                             <ul>
                                 <li><a href="#">Liste des modules</a></li>
                             </ul>
