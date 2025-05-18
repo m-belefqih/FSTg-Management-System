cee5392729bbf5376a37bacc8852589d4200bb37
BELEFQIH Mohammed <mblq02@gmail.com>
2025-05-19 00:52:43 +0100

    Add deleteNotification function to handle notification deletions in notification.php

--- models/notification.php
+++ models/notification.php
@@ -11,6 +11,25 @@ function addNewNotification($id_module, $id_sender, $id_receiver): void
         $stmt = $conn->prepare($query);
         $stmt->execute([$id_module, $id_sender, $id_receiver, $currentDateTime]);
     } catch (PDOException $e) {
-        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/logs/error_log.txt', date('Y-m-d H:i:s') . " - Erreur lors de l'insertion : " . $e->getMessage() . "\n", FILE_APPEND);
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/logs/error_log.txt',
+            date('Y-m-d H:i:s') . " - Erreur lors de l'insertion de notification : " . $e->getMessage() . "\n",
+            FILE_APPEND
+        );
+    }
+}
+
+function deleteNotification($id_module): void
+{
+    global $conn;
+    $query = "DELETE FROM notification WHERE id_module = ?";
+
+    try {
+        $stmt = $conn->prepare($query);
+        $stmt->execute([$id_module]);
+    } catch (PDOException $e) {
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . '/FSTg-Management-System/logs/error_log.txt',
+            date('Y-m-d H:i:s') . " - Erreur lors de la suppression de notification : " . $e->getMessage() . "\n",
+            FILE_APPEND
+        );
     }
 }
