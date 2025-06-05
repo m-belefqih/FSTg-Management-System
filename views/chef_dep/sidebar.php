0d0a6a8e7f18f7fb6bcfa989837dead7a60a87c3
BELEFQIH Mohammed <mblq02@gmail.com>
2025-06-05 17:13:43 +0100

    Update sidebar.php of chef_dep

--- views/chef_dep/sidebar.php
+++ views/chef_dep/sidebar.php
@@ -26,23 +26,16 @@
                             <span>Menu Professeur</span>
                         </li>
                         <li>
-<!--                            <a href="/ENSAHify/views/professeur/choixModuleCours.php"><i class="fas fa-book"></i> <span> Cours</span></a>-->
                             <a href="#"><i class="fas fa-book"></i> <span> Cours</span></a>
                         </li>
                         <li>
-<!--                            <a href="/ENSAHify/views/professeur/module-view.php"><i class="fas fa-book-reader"></i><span> Liste des Modules</span></a>-->
                             <a href="#"><i class="fas fa-book-reader"></i><span> Liste des Modules</span></a>
                         </li>
                         <li>
-<!--                            <a href="/ENSAHify/views/professeur/choixModuleNotes.php"><i class="fas fa-clipboard"></i> <span> Notes</span></a>-->
                             <a href="#"><i class="fas fa-clipboard"></i> <span> Notes</span></a>
                         </li>
                         <li>
-<!--                            <a href="/ENSAHify/views/professeur/timetable.php"><i class="fas fa-calendar-day"></i> <span> Emploi du Temps</span></a>-->
                             <a href="#"><i class="fas fa-calendar-day"></i> <span> Emploi du Temps</span></a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fas fa-address-card"></i><span> Profil</span></a>
                         </li>
                     <?php }?>
                 </ul>
