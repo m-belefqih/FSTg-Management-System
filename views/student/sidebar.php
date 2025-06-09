a597a11bc67bc36219c0f2c2ea7750d67361d90a
BELEFQIH Mohammed <mblq02@gmail.com>
2025-06-09 18:48:20 +0100

    Add sidebar.php for main navigation menu

---
+++ views/student/sidebar.php
@@ -0,0 +1,35 @@
+<!-- Custom style CSS -->
+<style>
+    .sidebar-menu > ul > li > a:hover {
+        color: #C17900; !important;
+    }
+</style>
+<div class="sidebar" id="sidebar">
+    <div>
+        <div class="sidebar-inner slimscroll">
+            <div id="sidebar-menu" class="sidebar-menu">
+                <ul>
+                    <li class="menu-title">
+                        <span>Menu Principal</span>
+                    </li>
+                    <li>
+                        <a href="/FSTg-Management-System/views/student/home.php"><i class="fas fa-home"></i><span> Accueil</span></a>
+                    </li>
+                    <li>
+                        <a href="/FSTg-Management-System/views/student/attachment_management/index.php"><i class="fas fa-book"></i> <span> Cours</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fas fa-book-reader"></i><span> Modules</span></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fas fa-clipboard"></i><span> Notes</span></a>
+                    </li>
+                    <li>
+                        <a href="/FSTg-Management-System/views/student/timetable_management/index.php?action=list"><i class="fas fa-calendar-day"></i><span> Emploi du temps</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
